ca9324e20a4ccb823ec5ee1322e469b80e77a6ca
Dtakuto <truetakuto2023@gmail.com>
2025-03-06 00:35:43 -0600

    Compute the upper tanget and the lower tanget

--- ConvexHullDC.java
+++ ConvexHullDC.java
@@ -18,7 +18,66 @@ class ConvexHullDC {
     }
 
     private static List<Point> mergeHulls(List<Point> leftHull, List<Point> rightHull) {
-        return null; 
+        int inda = findRightmost(leftHull);
+        int indb = findLeftmost(rightHull);
+
+        int upperLeft = inda;
+        int upperRight = indb;
+
+        //Computes the upper tangent 
+        while (true){
+            boolean upperTangetDone = false;
+
+            while(Point.crossProduct(rightHull.get(upperRight), leftHull.get(upperLeft), leftHull.get((upperLeft + 1) % leftHull.size())) > 0) {
+                upperLeft = (upperLeft + 1) % leftHull.size();
+                upperTangetDone = true;
+            }
+
+            while(Point.crossProduct(leftHull.get(upperLeft), rightHull.get(upperRight), rightHull.get((rightHull.size() + upperRight + 1 ) % rightHull.size())) < 0){
+                upperRight = (rightHull.size() + upperRight - 1) % rightHull.size();
+                upperTangetDone = true;
+            }
+            if (!upperTangetDone) break;
+        }
+
+        int lowLeft = inda;
+        int lowRight = indb; 
+
+        //Computes the lower tanget
+        while (true){
+            boolean lowerTangetDone = false;
+
+            while(Point.crossProduct(rightHull.get(lowRight), leftHull.get(lowLeft), leftHull.get((lowLeft + 1) % leftHull.size())) > 0) {
+                lowLeft = (lowLeft + 1) % leftHull.size();
+                lowerTangetDone = true;
+            }
+
+            while(Point.crossProduct(leftHull.get(lowLeft), rightHull.get(lowRight), rightHull.get((rightHull.size() + lowRight + 1 ) % rightHull.size())) < 0){
+                lowLeft = (rightHull.size() + upperRight - 1) % rightHull.size();
+                lowerTangetDone = true;
+            }
+            if (!lowerTangetDone) break;
+        }
+
+        List <Point> mergeList = new ArrayList<>(); 
+
+        int i = upperLeft;
+        while (i != lowLeft){
+            mergeList.add(leftHull.get(i));
+            i = (i + 1) % leftHull.size();
+        }
+
+        mergeList.add(leftHull.get(lowLeft));
+
+        i = lowRight;
+        while (i != upperRight) {
+            mergeList.add(rightHull.get(i));
+            i = (i + 1) % rightHull.size();
+        }
+
+        mergeList.add(rightHull.get(upperRight));
+
+        return mergeList;
     }
 
     private static int findRightmost(List<Point> hull) {
